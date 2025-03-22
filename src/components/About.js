35025df4a750a3c0872ce60c61a279ece525a1a3
Shubhamprogrammar <mauryashubham12349@gmail.com>
2025-03-22 16:07:10 +0530

    About changed

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,12 @@ function About() {
   return (
     <>
       <div className="container about mt-5">
-        <h2 className='text-center' style={{color: '#86003C'}}>🤍Hearts🤍</h2>
+        <h2 className='text-center' style={{ color: '#86003C' }}>🤍Hearts🤍</h2>
         <p><strong>Open Your Hearts</strong> is a community-driven initiative dedicated to spreading kindness, empathy, and support to those in need.</p>
         <p><strong>Open Your Hearts</strong> allows users to create, display, and organize hearts in a clear and visually appealing manner.</p>
         <p>The platform is ideal for Poet, professionals, teams, and anyone who needs to convey message without overwhelming their audience. With customizable formats and easy sharing options, <strong>Open Your Hearts</strong> makes it simple to keep everyone informed and on the same page.</p>
         <p>At Open Your Hearts, we aim to create a ripple effect of kindness that uplifts communities and brings people together. By empowering individuals to take action, we hope to build a more compassionate world where everyone feels valued and cared for.</p>
-<p><i><center>Join us in opening your heart to those around you, and together, we can make a difference.</center></i></p>
+        <p><i><center>Join us in opening your heart to those around you, and together, we can make a difference.</center></i></p>
       </div>
     </>
   )
